d1f9bfb969d422a558c145f1272e37d3c72d246c
Weikai Wang <wc563388548@gmail.com>
2020-07-20 21:16:54 +0900

    Installing and setting up basic webpack with html-webpack-plugin and webpack-dev-server

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const appSrc = path.resolve(__dirname, '../src');
+const appDist = path.resolve(__dirname, '../dist');
+const appPublic = path.resolve(__dirname, '../public');
+const appIndex = path.resolve(appSrc, 'index.js');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: appPublic,
+    hot: true,
+    host: 'localhost',
+    port: 8998,
+    historyApiFallback: true,
+    // displays error on browser
+    overlay: true,
+    inline: true,
+    // print error info
+    stats: 'errors-only',
+    // set up proxy
+    proxy: {
+      '/api': {
+        changeOrigin: true,
+        target: 'https://easy-mock.com/mock/5c2dc9665cfaa5209116fa40/example',
+        pathRewrite: {
+            '^/api/': '/'
+        }
+      }
+    }
+  },
+  entry: appIndex,
+  output: {
+    filename: 'public/js/[name].[hash:8].js',
+    path: appDist,
+    publicPath: '/'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: appHtml,
+      filename: 'index.html'
+    })
+  ]
+}
