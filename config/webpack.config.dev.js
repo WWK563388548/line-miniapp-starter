d56d36b18df1ec48230e8d362f4097c29772fa7d
Weikai Wang <wc563388548@gmail.com>
2020-07-20 21:18:56 +0900

    fix error

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const appSrc = path.resolve(__dirname, '../src');
 const appDist = path.resolve(__dirname, '../dist');
