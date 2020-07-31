8c04668aa358be63a0486c5d9d74c22286a4eccf
Weikai Wang <wc563388548@gmail.com>
2020-07-31 12:25:09 +0900

    Adding global style(reset style)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ResetStyle } from './globalStyles'
 
 import Home from 'pages/Home';
 import Settings from 'pages/Settings';
 import Display from 'pages/Display';
 import NotFound from 'pages/NotFound';
 
-import * as styles from './app.scss';
+// import * as styles from './app.scss';
 
 const App = (props: any) => {
   return (
-    <div className={styles.app}>
+    <div>
+      <ResetStyle />
       <Switch>
         <Route path='/settings' component={ Settings } />
         <Route path='/display' component={ Display } />
